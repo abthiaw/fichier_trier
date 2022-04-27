494364cbd62bbea9eb3b6fa549e654ef0c44be28
benedictthiaw <78225508+benedictthiaw@users.noreply.github.com>
2022-04-27 08:59:04 +0200

    binaire main

--- test.py
+++ test.py
@@ -8,26 +8,55 @@
 # Enregistrer les champs dans la base de données
 import sys
 from pprint import pprint
+import re
 
 
-def read_file(filename: str):
-    import re
+def get_data_blocks(filename: str, step: int, start_mark: str) -> list:
 
     #file_1, file_2, file_3 = open(args[1]), open(args[3]), open(args[3])
     f = open(filename)
     object_read = f.readlines()
 
-    debut, fin, data_block = None, None, None
+    debut, fin, list_data_blocks = None, None, []
     for idx, line in enumerate(object_read):
         # debut de l'objet
-        r = re.search("Transfer attributes", line)
+        r = re.search(start_mark, line)
         if r is not None:
             debut = idx - 1
-            fin = idx + 103
+            fin = idx + step
             #print(object_read[idx - 1].strip())
-            data_block = object_read[debut:fin]
-            pprint(data_block)
+            list_data_blocks.append(object_read[debut:fin])
+    return list_data_blocks
+
+
+def fetch_value(field_name: str, block: str):
+    """
+    Cette fonction recupere la valeur des données basé sur le nom en entré(field_name)
+    dans un bloc de données (block)
+    @params :
+        field_name : le nom du champ a récuperer
+        block : le bloc des données
+    """
+    value = None
+    for line in block:
+        r = re.search(field_name, line)
+        if r is not None:
+            line_list = line[r.span()[0]:].split()
+            i = line_list.index("=")
+            if len(line_list) <= 2:
+                value = ""
+            else:
+                value = line_list[i+1]
+    return value
+
+    # return value
 
 
 if __name__ == "__main__":
-    read_file("/Users/gabinmberikongo/code/dev/test/recv.csv")
+    liste_element = get_data_blocks(
+        "/Users/gabinmberikongo/code/dev/test/recv.csv", step=103, start_mark="Transfer attributes")
+
+    list_a_recup = ["FTYPE", "FCODE"]
+    for block in liste_element:
+        for name in list_a_recup:
+            print(fetch_value(name, block))
